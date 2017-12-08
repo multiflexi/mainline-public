633f8af4574e5590e437dac801d0b83c7da283e3
Antoine Tenart <antoine.tenart@bootlin.com>
2017-12-08 14:48:51 +0100

    net: mvpp2: allow to assign a reserved memory region for DMA allocations
    This patch adds the possibility to reserve a DMA region in the device
    tree, and to assign this memory region to the PPv2 device for DMA
    allocations. This helps to avoid cases where the default CMA pool is to
    small in the kernel, by having a PPv2 specific pool we can control.
    
    Signed-off-by: Antoine Tenart <antoine.tenart@bootlin.com>

--- drivers/net/ethernet/marvell/mvpp2.c
+++ drivers/net/ethernet/marvell/mvpp2.c
@@ -28,6 +28,7 @@
 #include <linux/of_net.h>
 #include <linux/of_address.h>
 #include <linux/of_device.h>
+#include <linux/of_reserved_mem.h>
 #include <linux/phy.h>
 #include <linux/phylink.h>
 #include <linux/phy/phy.h>
@@ -9017,11 +9018,18 @@ static int mvpp2_probe(struct platform_device *pdev)
 			goto err_axi_clk;
 	}
 
+	/* Assign the reserved memory region to the device for DMA allocations,
+	 * if a memory-region phandle is found.
+	 */
+	if (dev_of_node(&pdev->dev))
+		of_reserved_mem_device_init_by_idx(&pdev->dev,
+						   pdev->dev.of_node, 0);
+
 	/* Initialize network controller */
 	err = mvpp2_init(pdev, priv);
 	if (err < 0) {
 		dev_err(&pdev->dev, "failed to initialize controller\n");
-		goto err_axi_clk;
+		goto err_mem_device;
 	}
 
 	/* Initialize ports */
@@ -9034,7 +9042,7 @@ static int mvpp2_probe(struct platform_device *pdev)
 	if (priv->port_count == 0) {
 		dev_err(&pdev->dev, "no ports enabled\n");
 		err = -ENODEV;
-		goto err_axi_clk;
+		goto err_mem_device;
 	}
 
 	/* Statistics must be gathered regularly because some of them (like
@@ -9062,6 +9070,8 @@ err_port_probe:
 			mvpp2_port_remove(priv->port_list[i]);
 		i++;
 	}
+err_mem_device:
+	of_reserved_mem_device_release(&pdev->dev);
 err_axi_clk:
 	clk_disable_unprepare(priv->axi_clk);
 
