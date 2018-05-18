1466f41eb24c5e8b94bd04deb469c2b1bca2b164
Antoine Tenart <antoine.tenart@bootlin.com>
2018-05-18 16:07:43 +0200

    net: mvpp2: increase the number of s/w threads to 9
    This patch sets the number of s/w threads to 9, its maximum value,
    instead of 8. This is not a fix as only 4 of the s/w threads were used
    so far (but more will be used in the future).
    
    Signed-off-by: Antoine Tenart <antoine.tenart@bootlin.com>

--- drivers/net/ethernet/marvell/mvpp2.c
+++ drivers/net/ethernet/marvell/mvpp2.c
@@ -879,7 +879,7 @@ enum mvpp2_prs_l3_cast {
 #define MVPP21_ADDR_SPACE_SZ		0
 #define MVPP22_ADDR_SPACE_SZ		SZ_64K
 
-#define MVPP2_MAX_THREADS		8
+#define MVPP2_MAX_THREADS		9
 #define MVPP2_MAX_QVECS			MVPP2_MAX_THREADS
 
 enum mvpp2_bm_pool_log_num {
